bbf6f241249006e35ff9748a4962bd316d5c8904
yvonnendutaw <yvonita7@gmail.com>
2016-05-08 20:02:12 +0300

    added script.py

---
+++ script.py
@@ -0,0 +1,13 @@
+#usr/bin/python
+import random
+import sys
+import os
+
+#printing
+print 'hello world'
+
+#declaring variables
+name = 'derik'
+print name
+
+#numbers,lists,strings,dictionaries,tuples --> data types
