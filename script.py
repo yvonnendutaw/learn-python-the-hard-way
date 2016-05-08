f1768b3ad6d0ed36f2b51d5a9b1dffc36cfeada8
yvonnendutaw <yvonita7@gmail.com>
2016-05-08 20:13:14 +0300

    data types introduction and printing

--- script.py
+++ script.py
@@ -11,3 +11,10 @@ name = 'derik'
 print name
 
 #numbers,lists,strings,dictionaries,tuples --> data types
+quote = '\"always remember you are unique"'
+multiline_quote = '''just like everyone else'''
+print quote + multiline_quote
+
+
+
+print ("%s %s %s" % ('i like the quote', quote,multiline_quote))
