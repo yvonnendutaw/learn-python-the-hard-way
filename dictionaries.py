7ae91608d49cf78fadea174d56cbbbfb517eaacb
yvonnendutaw <yvonita7@gmail.com>
2016-05-08 20:54:37 +0300

    adding dictionaries , dictionary methods

---
+++ dictionaries.py
@@ -0,0 +1,29 @@
+#usr/bin/python
+import random
+import sys
+import os
+
+super_villains = {
+    'captain cold' : 'leonard snart',
+    'mirror master': 'sam scudder',
+    'pied piper'   : 'thomas peterson'
+}
+
+#prints value for captain cold
+print super_villains['captain cold']
+
+#deletes mirror master key and value
+del super_villains['mirror master']
+
+#prints all keys and values
+print super_villains
+
+#changed value for one key
+super_villains['pied piper'] = 'harley rathaway'
+print super_villains['pied piper']
+
+#prints all keys without values
+print super_villains.keys()
+
+#prints all values without keys
+print super_villains.values()
