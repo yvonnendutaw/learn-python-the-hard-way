d60366b4147fb57d0b8c7b07dda32abcbcfbecb0
yvonnendutaw <yvonita7@gmail.com>
2016-05-22 14:49:49 +0300

    more variable and printing

---
+++ ex4.py
@@ -0,0 +1,22 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+print
+print
+print
+print
+print
+print
+"Let's talk about %s." % my_name
+"He's %d inches tall." % my_height
+"He's %d pounds heavy." % my_weight
+"Actually that's not too heavy."
+"He's got %s eyes and %s hair." % (my_eyes, my_hair)
+"His teeth are usually %s depending on the coffee." % my_teeth
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+my_age, my_height, my_weight, my_age + my_height + my_weight)
