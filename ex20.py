7e15a0b64f315355d966651c59f3c0d40f655a68
yvonnendutaw <yvonita7@gmail.com>
2016-05-22 18:34:18 +0300

    returning with functions

---
+++ ex20.py
@@ -0,0 +1,22 @@
+def add(a,b):
+    return a + b
+def subtract(a,b):
+    return a - b
+def multiply(a,b):
+    return a * b
+def divide(a, b):
+    return a / b
+
+#calls the functions
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
+
+# A puzzle for the extra credit, type it in anyway.
+print "Here is a puzzle."
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print "That becomes: ", what, "Can you do it by hand?"
